306ba0ad93ccf9842597ec8d24b3dfbbaef91bf6
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-05 11:09:44 +0200

    instal SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -28,3 +28,19 @@ function createGalleryImages (galleryItems) {
 // Элементы галереи созданы и добавлены в ul.gallery
 galleryList.insertAdjacentHTML('beforeend', imageItem);
 
+import SimpleLightbox from 'simplelightbox'; // импортируем библиотеку
+
+const galleryLinks = galleryList.querySelectorAll('.gallery__link'); // получаем все ссылки в галерее
+
+// проходимся по каждой ссылке и добавляем атрибуты
+galleryLinks.forEach((link) => {
+  link.setAttribute('data-slb-group', 'myGallery'); // группируем ссылки в одну галерею
+  link.setAttribute('href', link.querySelector('.gallery__image').getAttribute('src')); // указываем путь к большому изображению
+});
+
+// инициализируем библиотеку
+new SimpleLightbox('.gallery__link', {
+  captions: true, // показываем подписи к изображениям
+  captionsData: 'alt', // берем подписи из атрибута alt
+  captionDelay: 250, // задержка перед появлением подписи
+});
