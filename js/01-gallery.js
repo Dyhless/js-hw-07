cea15e81feea3028f9dc43666327a35142b3c5ac
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-05 14:43:46 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,74 +1,77 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-
+// Получаем ссылку на контейнер галереи
 const galleryList = document.querySelector('.gallery');
+
+// Создаем разметку с изображениями галереи на основе массива galleryItems
 const imageItem = createGalleryImages(galleryItems);
 
-//Создание и рендер разметки по массиву данных galleryItems
+// Функция для создания элементов галереи
 function createGalleryImages (galleryItems) {
    return galleryItems.map( ({ preview, original, description }) => {
       return `
          <li class="gallery__item">
-        <a
-          class="gallery__link"
-          href="#"
-        >
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </li>
+            <a class="gallery__link" href="#">
+               <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+            </a>
+         </li>
       `;
-   })
-   .join('');
+   }).join('');
 }
 
-// Элементы галереи созданы и добавлены в ul.gallery
+// Добавляем разметку с изображениями в контейнер галереи
 galleryList.insertAdjacentHTML('beforeend', imageItem);
 
+// Добавляем слушатель на клик по изображению галереи
 galleryList.addEventListener('click', onGalleryItem);
 
+// Обработчик клика по изображению галереи
 function onGalleryItem(evt) {
    evt.preventDefault();
-   
+
+   // Получаем ссылку на элемент, по которому кликнули
    const clickElement = event.target;
+
+   // Проверяем, что кликнули именно по изображению
    const isImage = clickElement.tagName === 'IMG';
 
    if (!isImage) {
       return;
    }
 
-  const originalUrl = clickElement.dataset.source;
-  
-  const modal = basicLightbox.create(`
-    <div class="modal">
-      <img src="${originalUrl}" alt="">
-    </div>
-  `);
+   // Получаем ссылку на оригинальное изображение
+   const originalUrl = clickElement.dataset.source;
 
-  const img = modal.element().querySelector('img');
-  img.src = originalUrl;
+   // Создаем модальное окно с оригинальным изображением
+   const modal = basicLightbox.create(`
+      <div class="modal">
+         <img src="${originalUrl}" alt="">
+      </div>
+   `);
 
+   // Получаем ссылку на изображение внутри модального окна
+   const img = modal.element().querySelector('img');
+
+   // Устанавливаем источник изображения в соответствии с оригинальным изображением
+   img.src = originalUrl;
+
+   // Отображаем модальное окно
    modal.show();
-  console.log(originalUrl);
-  
-// Добавляем слушатель на закрытие модалки на esc кнопку
-  document.addEventListener('keydown', onModalKeyDown);
 
-  function onModalKeyDown(evt) {
-    if (evt.code === 'Escape') {
-      modal.close();
+   // Выводим ссылку на оригинальное изображение в консоль
+   console.log(originalUrl);
 
-      // удаляем слушатель после того как модалка закрыватся
-      document.removeEventListener('keydown', onModalKeyDown);
-    }
-  }
+   // Добавляем слушатель на закрытие модального окна по нажатию клавиши Esc
+   document.addEventListener('keydown', onModalKeyDown);
+
+   // Обработчик нажатия клавиши Esc при открытом модальном окне
+   function onModalKeyDown(evt) {
+      if (evt.code === 'Escape') {
+         // Закрываем модальное окно
+         modal.close();
+
+         // Удаляем обработчик нажатия клавиши Esc после закрытия модального окна
+         document.removeEventListener('keydown', onModalKeyDown);
+      }
+   }
 }
-
-
-
-
